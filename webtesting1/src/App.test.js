69c99a043108e0e7ea14c315b7a9ce03d913b448
dislersd <dislersd@gmail.com>
2019-04-09 10:14:29 -0700

    created test for App component

--- webtesting1/src/App.test.js
+++ webtesting1/src/App.test.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { render } from 'react-testing-library'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render } from "react-testing-library";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("<App/>", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('renders successfully', () => {
-  render(<App/>)
+  it("renders successfully", () => {
+    render(<App />);
+  });
+
+  it('renders hello world', () => {
+    const { getByText, queryByText } = render(<App/>);
+
+
+    //getByText(/Hello World/)
+    expect(queryByText(/hello world/i)).not.toBeNull();
+  });
 });
